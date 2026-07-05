10b3d12680500df39f0019aebc4e3bc5208f78ac
kiawu07 <alwalidkiawu@gmail.com>
2026-07-05 11:01:14 -0500

    Changes to Client class

--- bank_simulator/client.cpp
+++ bank_simulator/client.cpp
@@ -1,1 +1,33 @@
 /*Implementation of the client class*/
+#include "client.hpp"
+#include <iostream>
+using namespace std;
+
+Client::Client(string name, int age, int SSN, string address, string nationality){
+    this->name = name;
+    this->age = age;
+    this->SSN = SSN;
+    this->address = address;
+    this->nationality = nationality;
+}
+
+//Getters
+string Client::getName(){
+    return name;
+}
+
+int Client::getAge(){
+    return age;
+}
+
+int Client::getSSN(){
+    return SSN;
+}
+
+string Client::getAddress(){
+    return address;
+}
+
+string Client::getNationality(){
+    return nationality;
+}
--- bank_simulator/client.hpp
+++ bank_simulator/client.hpp
@@ -4,56 +4,29 @@ Alwalid Kiawu, July 2, 2026, 5:10 PM
 
 #include <iostream>
 #include <string>
+
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
+
 
 class Client{
 
     private:
         string name;
-        int account_number;
-        int pin;
-        int balance;
-        string account_type;
-        int withdrawal_limit;
-        int deposit_limit;
-        int stock_value;
-    
+        int age;
+        int SSN;
+        string address;
+        string nationality;
+
     public:
+        Client(string name, int age, int SSN, string address, string nationality);
 
-        Client(){
-            name = "";
-            account_number = 0;
-            pin = 0;
-            balance = 0;
-            account_type = "";
-            withdrawal_limit = 0;
-            deposit_limit = 0;
-            stock_value = 0;    
-        }
-
-        //setters
-        void set_name(string n){};
-
-        void set_account_number(){};
-
-        void set_pin(){};
-
-        void set_balance(){};
-
-        void set_account_type(){};
-
-        void set_stock_value(){};
-
-        //getters
-        string get_name(){};
-
-        int get_account_number(){};
-
-        int get_balance(){};
-
-        int get_stock_value(){};
-
+        //Getters
+        string getName();
+        int getAge();
+        int getSSN();
+        string getAddress();
+        string getNationality(); 
 };
 
 
